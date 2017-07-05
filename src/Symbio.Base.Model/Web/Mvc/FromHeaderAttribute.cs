a69ba16ed62d0764816dddc4e7d1aa6dffe138c3
Thomas Hauser <thomas.hauser@p-und-z.de>
2017-07-05 16:10:42 +0200

    Add FromHeaderAttribute and FromHeaderBinding classes.

---
+++ src/Symbio.Base.Model/Web/Mvc/FromHeaderAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace Symbio.Base.Model.Web.Mvc
+{
+    public class FromHeaderAttribute : ParameterBindingAttribute
+    {
+        #region Constructors
+
+        public FromHeaderAttribute(string headerName)
+        {
+            _headerName = headerName;
+        }
+
+        #endregion Constructors
+
+        #region Fields
+
+        private readonly string _headerName;
+
+        #endregion Fields
+
+        #region Methods
+
+        #region Public Methods
+
+        public override HttpParameterBinding GetBinding(HttpParameterDescriptor parameter)
+        {
+            return new FromHeaderBinding(parameter, _headerName);
+        }
+
+        #endregion Public Methods
+
+        #endregion Methods
+    }
+}
---
+++ src/Symbio.Base.Model/Web/Mvc/FromHeaderBinding.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Metadata;
+
+namespace Symbio.Base.Model.Web.Mvc
+{
+    public class FromHeaderBinding : HttpParameterBinding
+    {
+        #region Constructors
+
+        public FromHeaderBinding(HttpParameterDescriptor parameter, string headerName)
+            : base(parameter)
+        {
+            if (string.IsNullOrEmpty(headerName))
+                throw new ArgumentNullException("headerName");
+
+            _headerName = headerName;
+        }
+
+        #endregion Constructors
+
+        #region Fields
+
+        private readonly string _headerName;
+
+        #endregion Fields
+
+        #region Methods
+
+        #region Public Methods
+
+        public override Task ExecuteBindingAsync(ModelMetadataProvider metadataProvider, HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+            if (actionContext.Request.Headers.TryGetValues(_headerName, out IEnumerable<string> values))
+                actionContext.ActionArguments[Descriptor.ParameterName] = values.FirstOrDefault();
+
+            var taskSource = new TaskCompletionSource<object>();
+            taskSource.SetResult(null);
+            return taskSource.Task;
+        }
+
+        #endregion Public Methods
+
+        #endregion Methods
+    }
+}
